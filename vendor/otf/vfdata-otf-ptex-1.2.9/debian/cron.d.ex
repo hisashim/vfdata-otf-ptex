2ac5f0e16f5fcd9d86fea2199a5f76b4c3dc2d50
Hisashi Morita <hisashim@workbook.org>
2007-06-19 15:35:07 +0000

    Remove unnecessary example files (vfdata-otf-ptex/debian/*.ex).

--- vendor/otf/vfdata-otf-ptex-1.2.9/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the vfdata-otf-ptex package
-#
-0 4	* * *	root	vfdata-otf-ptex_maintenance
--- vendor/otf/vfdata-otf-ptex-1.2.9/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vfdata-otf-ptex):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="vfdata-otf-ptex" command="/usr/bin/vfdata-otf-ptex"
