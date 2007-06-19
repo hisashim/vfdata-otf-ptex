476daa4ec11d80ae0ad0554c33839e4d8e2b5dc5
Hisashi Morita <hisashim@workbook.org>
2007-06-19 18:04:58 +0000

    Improvement: vfdata-otf-ptex is more compliant with Debian policy, changing section to contrib/tex, removing unnecessary files, adding additional example files for testing, etc. (thanks to kmuto)

--- vendor/otf/pbuilder-hooks-test/test.sh
+++ vendor/otf/pbuilder-hooks-test/test.sh
@@ -36,48 +36,21 @@ else
   ls -la /etc/texmf/texmf.d/
 fi
 update-texmf
-echo '% Non-embedding font map, which works without actual font data.
-% Source: http://oku.edu.mie-u.ac.jp/~okumura/texwiki/?OTF
-rml          H                Ryumin-Light
-rmlv         V                Ryumin-Light
-gbm          H                GothicBBB-Medium
-gbmv         V                GothicBBB-Medium
-hminr-h      H                Ryumin-Light
-hminr-v      V                Ryumin-Light
-otf-ujmr-h   UniJIS-UTF16-H   Ryumin-Light
-otf-ujmr-v   UniJIS-UTF16-V   Ryumin-Light
-otf-cjmr-h   Adobe-Japan1-6   Ryumin-Light
-otf-cjmr-v   Identity-V       Ryumin-Light
-hgothr-h     H                GothicBBB-Medium
-hgothr-v     V                GothicBBB-Medium
-otf-ujgr-h   UniJIS-UTF16-H   GothicBBB-Medium
-otf-ujgr-v   UniJIS-UTF16-V   GothicBBB-Medium
-otf-cjgr-h   Adobe-Japan1-6   GothicBBB-Medium
-otf-cjgr-v   Identity-V       GothicBBB-Medium
-' >> /etc/texmf/dvipdfm/my-pseudo-otf.map
-echo "### /etc/texmf/dvipdfm/my-pseudo-otf.map"
-cat /etc/texmf/dvipdfm/my-pseudo-otf.map
-mktexlsr
+cp /usr/share/doc/vfdata-otf-ptex/examples/my-pseudo-otf.map ./
+echo "### my-pseudo-otf.map"
+cat my-pseudo-otf.map
 
 echo "### Processing LaTeX document using OTF, without actual font data..."
-echo '\documentclass{jbook}
-\usepackage{otf}
-\begin{document}
-OpenTypeフォントを使うためのOTFパッケージのテストです。 \\
-森\UTF{9DD7}外（區＋鳥） \\
-内田百\UTF{9592}（門＋月） \\
-\end{document}
-' >> myotftest.tex
+cp /usr/share/doc/vfdata-otf-ptex/examples/myotftest.tex ./
 echo "### myotftest.tex"
-platex myotftest.tex && dvipdfmx -f my-pseudo-otf myotftest.dvi
+platex myotftest.tex && dvipdfmx -f my-pseudo-otf.map myotftest.dvi
 echo "### Copying myotftest.pdf to /var/cache/pbuilder/result..."
 cp myotftest.pdf /var/cache/pbuilder/result
 
 echo "### Cleaning up..."
-rm /etc/texmf/dvipdfm/my-pseudo-otf.map
-mktexlsr
-rm myotftest.{tex,aux,log,dvi,pdf}
+rm my-pseudo-otf.map myotftest.{tex,aux,log,dvi,pdf}
 
+echo "### Testing uninstallation..."
 dpkg --remove vfdata-otf-ptex
 dpkg --install /var/cache/pbuilder/result/${DEBFILE_BASENAME}_all.deb
 dpkg --purge vfdata-otf-ptex
