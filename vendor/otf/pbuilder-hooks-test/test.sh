c15cd8a082943944abb7354c6550b84a4435eee2
Hisashi Morita <hisashim@workbook.org>
2008-03-09 20:00:28 +0000

    vfdata-otf-ptex: Fix: test.sh should set up awk and fakeroot before installing build requirements.

--- vendor/otf/pbuilder-hooks-test/test.sh
+++ vendor/otf/pbuilder-hooks-test/test.sh
@@ -7,12 +7,16 @@ if [ -n "$1" ]
 fi
 
 echo "### Editing apt lines..."
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cp -v /etc/apt/sources.list /etc/apt/sources.list.bak
 echo 'deb http://cdn.debian.net/debian etch main contrib non-free' >> /etc/apt/sources.list
 echo "### /etc/apt/sources.list"
 diff -u /etc/apt/sources.list.bak /etc/apt/sources.list
 apt-get update
 apt-get update
+echo "### Making sure fakeroot is executable..."
+/usr/sbin/update-alternatives --auto fakeroot
+echo "### Making sure awk is executable, which is necessary for libpaper1 configuration..."
+/usr/sbin/update-alternatives --auto awk
 
 echo "### Installing vfdata-otf-ptex and requirements..."
 dpkg -i /var/cache/pbuilder/result/${DEBFILE_BASENAME}_all.deb
@@ -23,7 +27,7 @@ dpkg -l | grep '^ii'
 echo "### Setting up CMAPs and font maps..."
 if [ -f /etc/texmf/texmf.d/50dvipdfmx.cnf ]
 then
-  cp /etc/texmf/texmf.d/50dvipdfmx.cnf /etc/texmf/texmf.d/50dvipdfmx.cnf.bak
+  cp -v /etc/texmf/texmf.d/50dvipdfmx.cnf /etc/texmf/texmf.d/50dvipdfmx.cnf.bak
   echo 'CMAPINPUTS = .;/usr/share/fonts/cmap//' >> /etc/texmf/texmf.d/50dvipdfmx.cnf
   echo "### /etc/texmf/texmf.d/50dvipdfmx.cnf"
   diff -u /etc/texmf/texmf.d/50dvipdfmx.cnf.bak /etc/texmf/texmf.d/50dvipdfmx.cnf
@@ -36,24 +40,19 @@ else
   ls -la /etc/texmf/texmf.d/
 fi
 update-texmf
-cp /usr/share/doc/vfdata-otf-ptex/examples/my-pseudo-otf.map ./
+cp -v /usr/share/doc/vfdata-otf-ptex/examples/my-pseudo-otf.map ./
 echo "### my-pseudo-otf.map"
 cat my-pseudo-otf.map
 
 echo "### Processing LaTeX document using OTF, without actual font data..."
-cp /usr/share/doc/vfdata-otf-ptex/examples/myotftest.tex ./
+cp -v /usr/share/doc/vfdata-otf-ptex/examples/myotftest.tex ./
 echo "### myotftest.tex"
 platex myotftest.tex && dvipdfmx -f my-pseudo-otf.map myotftest.dvi
 echo "### Copying myotftest.pdf to /var/cache/pbuilder/result..."
-cp myotftest.pdf /var/cache/pbuilder/result
+cp -v myotftest.pdf /var/cache/pbuilder/result
 
 echo "### Cleaning up..."
-rm my-pseudo-otf.map
-rm myotftest.tex
-rm myotftest.aux
-rm myotftest.log
-rm myotftest.dvi
-rm myotftest.pdf
+rm -v my-pseudo-otf.map myotftest.tex myotftest.aux myotftest.log myotftest.dvi myotftest.pdf
 
 echo "### Testing uninstallation..."
 dpkg --remove vfdata-otf-ptex
